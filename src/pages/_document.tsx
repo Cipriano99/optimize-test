73d0880db77e1c279e6672aa63560cd548066c03
Cipriano99 <samuelcipriano99@gmail.com>
2023-02-15 12:54:52 -0300

    import google analytics & optimizer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { GoogleAnalyticsBase } from '../assets/tags/GoogleAnalyticsBase'
+import { GoogleOptimizer } from '../assets/tags/GoogleOptimizer'
 
 export default function Document() {  
   return (
     <Html lang="pt-br">
-      <Head></Head>
+      <Head>
+        <GoogleAnalyticsBase />
+        <GoogleOptimizer />
+      </Head>
       <body>
         <Main />
         <NextScript />
